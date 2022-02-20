d038be134bb9eb81bed7e350675aae5c89a36bf0
Tim <timpyez@gmail.com>
2022-02-20 16:58:54 +0000

    support for staffs and staves

--- src/HUDManager.cpp
+++ src/HUDManager.cpp
@@ -16,35 +16,44 @@ using namespace std;
 	return object;
 }
 
-
-
-
 bool ValidCastType(RE::PlayerCharacter* player, RE::TESForm* form)
 {
 	if (form && form->Is(RE::FormType::Spell)) {
-		auto spell = static_cast<RE::SpellItem*>(form);
+		auto spell = form->As<RE::SpellItem>();
 		return IsCasting(player, spell) && (spell->GetDelivery() == RE::MagicSystem::Delivery::kAimed) && (spell->GetCastingType() != RE::MagicSystem::CastingType::kConcentration);
 	}
 	return false;
 }
 
-using ATTACK_STATE = RE::ATTACK_STATE_ENUM;
+bool ValidStaffType(RE::TESForm* form, RE::ActorMagicCaster* magicCaster)
+{
+	if (form && form->Is(RE::FormType::Weapon)) {
+		auto weapon = form->As<RE::TESObjectWEAP>();
+		if (weapon->IsStaff()) {
+			RE::EnchantmentItem* enchantment = weapon->formEnchanting;
+			if (enchantment) {
+				return (magicCaster->currentSpell) && (enchantment->GetDelivery() == RE::MagicSystem::Delivery::kAimed) && (enchantment->GetCastingType() != RE::MagicSystem::CastingType::kConcentration);
+			}
+		}
+	}
+	return false;
+}
 
 bool ValidAttackType(RE::PlayerCharacter* player)
 {
-
 	auto attackState = player->actorState1.meleeAttackState;
-	return attackState == ATTACK_STATE::kBowAttached || attackState == ATTACK_STATE::kBowDrawn;
+	return attackState == RE::ATTACK_STATE_ENUM::kBowAttached || attackState == RE::ATTACK_STATE_ENUM::kBowDrawn;
 }
 
 void HUDManager::UpdateCrosshair(RE::PlayerCharacter* player)
 {
 	auto pickData = RE::CrosshairPickData::GetSingleton()->target;
-
 	auto leftHand = player->GetEquippedObject(true);
 	auto rightHand = player->GetEquippedObject(false);
+	auto leftHandCaster = player->magicCasters[0];
+	auto rightHandCaster = player->magicCasters[1];
 
-	auto isVisible = pickData || ValidAttackType(player) || ValidCastType(player, leftHand) || ValidCastType(player, rightHand);
+	auto isVisible = pickData || ValidStaffType(leftHand, leftHandCaster) || ValidStaffType(rightHand, rightHandCaster) || ValidAttackType(player) || ValidCastType(player, leftHand) || ValidCastType(player, rightHand);
 
 	if (isVisible) {
 		alpha += prevDelta * (maxOpacity / fadeSpeed);
